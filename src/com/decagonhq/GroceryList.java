8099155cd617332f772d75f6ad3333bba7aa31a5
Jideofor Nwoke <njusticej@gmail.com>
2020-01-11 19:09:31 +0100

    add getter, add onfile method to do the job of find item, made find_item, remove item that takes two parameters and modify item that took position private methods, overloaded modify items and remove items

--- src/com/decagonhq/GroceryList.java
+++ src/com/decagonhq/GroceryList.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<>();
 
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
     public void addGroceryItem(String item){
         groceryList.add(item);
     }
@@ -16,29 +21,43 @@ public class GroceryList {
         }
     }
 
-    public void modifyGroceryItem(int position, String newItem){
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItems(currentItem);
+        if(position >= 0){
+            modifyGroceryItem(position, newItem);
+        }
+
+    }
+
+    private void modifyGroceryItem(int position, String newItem){
         groceryList.set(position,newItem);
         System.out.println("Grocery item " + (position+1) + " has been modified");
 
     }
 
-    public void removeGroceryItem(int position){
-        String theItem = groceryList.get(position);
-        groceryList.remove(position);
-        System.out.println(theItem + " was removed");
-    }
-
-    public String findItems(String searchItem){
-        boolean exists = groceryList.contains(searchItem);
-        int position = groceryList.indexOf(searchItem);
+    public void removeGroceryItem(String Item){
+        int position = findItems(Item);
         if(position >= 0){
-
-            String itemm = groceryList.get(position);
-            return itemm;
+            removeGroceryItem(position);
         }
-        return null;
     }
-    public void modifyGroceryItem2(){
-        System.out.println("modified again");
+
+    private void removeGroceryItem(int position){
+
+        groceryList.remove(position);
+
     }
+
+    private int findItems(String searchItem){
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem){
+        int position = findItems(searchItem);
+        if(position >= 0){
+            return true;
+        }
+        return false;
+    }
+
 }
